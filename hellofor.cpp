b2914fc964d0b17bbbaee029a4fe9ad8850b2c46
madamovic <madamovickrivaja@gmail.com>
2015-10-06 23:49:09 +0200

    Using FOR and IF.

---
+++ hellofor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i=1; i<=10; i++) {
+		cout << i << " hello world" << endl ;
+		if ( i == 7 ) {
+			cout << "that was lucky!" << endl ;
+		} else {
+			cout << endl ;
+	}
+}
+}
